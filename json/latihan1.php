e9c9dc694eaf38c7a679e37acda82a5ef09b1837
Agung Wicaksono <agungthebesttop@gmail.com>
2020-01-05 13:23:35 +0700

    Rest Api Pizza Hut

---
+++ json/latihan1.php
@@ -0,0 +1,22 @@
+<?php
+  $mahasiswa = [
+    [
+      "nama"  => "Agung Wicaksono",
+      "nrp"   => "031291",
+      "email" => "agungthebesttop@gmail.com"
+    ],
+    [
+      "nama"  => "Agung Wicak",
+      "nrp"   => "031291",
+      "email" => "agungthebesttop@gmail.com"
+    ]
+  ];
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+
+
+
+
+ ?>               
